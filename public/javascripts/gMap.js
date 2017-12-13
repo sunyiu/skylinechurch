b8eaef8e3fc99502f8f5e6ac0b3db52974f7bd4f
Sunny Wong <swong@scisn.net>
2017-12-13 13:35:07 -0800

    add in Google map

---
+++ public/javascripts/gMap.js
@@ -0,0 +1,32 @@
+function initMap() {
+    var myLatLng = { lat: 49.16152, lng: -123.159214 };
+
+    var map = new google.maps.Map(document.getElementById('gMap'), {
+        zoom: 17,
+        center: myLatLng,
+        disableDefaultUI: true
+    });
+
+    var contentString = '<div id="content">' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<h2 id="firstHeading" class="firstHeading">Skyline Church</h2>' +
+        '<div id="bodyContent">' +
+        '<p></p>' +
+        '<p><h5></h5></p>'
+    '</div></div>';
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    var icon = 'https://maps.google.com/mapfiles/kml/pal2/icon10.png';
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        icon: icon,
+        map: map,
+        title: 'Skyline Church'
+    });
+    marker.addListener('click', function () { infowindow.open(map, marker); });
+    infowindow.open(map, marker);
+}
